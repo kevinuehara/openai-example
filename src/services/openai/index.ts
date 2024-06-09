4cbcdeb3019e5b0217bdffa63f10c4770948673a
galois <alexdf0001@gmail.com>
2024-06-09 16:30:50 -0300

    correcoes_iniciais

--- src/services/openai/index.ts
+++ src/services/openai/index.ts
@@ -1,24 +1,27 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
-const configuration = new Configuration({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-});
 
 const MESSAGE_COMPLEMENT =
   "formatado em json array com o campo name, representando o nome do lugar e a localização. E o campo location sendo um array com as coordenadas";
 
 export class Openai {
   static async listModels() {
-    const openai = new OpenAIApi(configuration);
-    const response = await openai.listModels();
+    const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
+});
+    const response = await openai.models.list();
     return response;
   }
 
   static async getLocations(message: string) {
-    const openai = new OpenAIApi(configuration);
+    const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
+});
     try {
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
+      const response = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
         prompt: `${message} ${MESSAGE_COMPLEMENT}`,
         max_tokens: 500,
         temperature: 0,
